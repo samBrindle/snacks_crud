a4237013115b6c67adeb5c6f69a574e975c511f0
Sam Brindle <brindlejsam@gmail.com>
2022-08-25 21:29:21 -0700

    added css tailwind to my project

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ThingListView, ThingDetailView, ThingCreateView, ThingUpdateView, ThingDeleteView
+from .views import SnackListView, SnackDetailView, SnackCreateView, SnackUpdateView, SnackDeleteView
 
 urlpatterns = [
-    path("list/", ThingListView.as_view(), name="snack_list"),
-    path("detail/<int:pk>/", ThingDetailView.as_view(), name="snack_detail"),
-    path("create/", ThingCreateView.as_view(), name="snack_create"),
-    path("update/<int:pk>/", ThingUpdateView.as_view(), name="snack_update"),
-    path("delete/<int:pk>/", ThingDeleteView.as_view(), name="snack_delete"),
+    path("list/", SnackListView.as_view(), name="snack_list"),
+    path("detail/<int:pk>/", SnackDetailView.as_view(), name="snack_detail"),
+    path("create/", SnackCreateView.as_view(), name="snack_create"),
+    path("update/<int:pk>/", SnackUpdateView.as_view(), name="snack_update"),
+    path("delete/<int:pk>/", SnackDeleteView.as_view(), name="snack_delete"),
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -3,29 +3,29 @@ from .models import Snack
 from django.urls import reverse_lazy
 
 
-class ThingListView(ListView):
+class SnackListView(ListView):
     template_name = "snack_list.html"
     model = Snack
 
 
-class ThingDetailView(DetailView):
+class SnackDetailView(DetailView):
     template_name = "snack_detail.html"
     model = Snack
 
 
-class ThingCreateView(CreateView):
+class SnackCreateView(CreateView):
     template_name = "snack_create.html"
     model = Snack
     fields = ["name","purchaser","description"]
 
 
-class ThingUpdateView(UpdateView):
+class SnackUpdateView(UpdateView):
     template_name = "snack_update.html"
     model = Snack
     fields = ["name", "description"]
 
 
-class ThingDeleteView(DeleteView):
+class SnackDeleteView(DeleteView):
     template_name = "snack_delete.html"
     model = Snack
     success_url = reverse_lazy("snack_list")
